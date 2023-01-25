68f95306fd6315570ab8fddc288643ae415c581e
ocavue <ocavue@gmail.com>
2023-01-25 15:35:58 +0800

    test: add more tests for indent

--- src/commands/indent-list.spec.ts
+++ src/commands/indent-list.spec.ts
@@ -69,4 +69,52 @@ describe('indentList', () => {
       `
     )
   })
+
+  it.skip('can keep the indentation of sub list nodes', () => {
+    const editor = t.add(
+      t.markdown`
+        - A1
+        - A2
+        - A3<cursor>
+          - B1 
+          - B2 
+          - B3
+      `
+    )
+    editor.commands.indentList()
+    expect(editor.state).toEqualRemirrorState(
+      t.markdown`
+        - A1
+        - A2
+          - A3<cursor>
+          - B1 
+          - B2 
+          - B3 
+      `
+    )
+  })
+
+  it.skip('can keep the indentation of sub list nodes when moving multiple list', () => {
+    const editor = t.add(
+      t.markdown`
+        - A1
+        - <start>A2
+        - A3<end>
+          - B1 
+          - B2 
+          - B3 
+      `
+    )
+    editor.commands.indentList()
+    expect(editor.state).toEqualRemirrorState(
+      t.markdown`
+        - A1
+          - <start>A2
+          - A3<end>
+          - B1 
+          - B2 
+          - B3 
+      `
+    )
+  })
 })
