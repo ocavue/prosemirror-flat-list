9f20e0298501de8db87fdb95ed7dbcf33ed93243
ocavue <ocavue@gmail.com>
2023-01-28 01:28:26 +0800

    test: add a complex indent case

--- src/commands/indent-list.spec.ts
+++ src/commands/indent-list.spec.ts
@@ -193,4 +193,30 @@ describe('indentList', () => {
       `,
     )
   })
+
+  it.skip('can handle some complex nested lists', () => {
+    t.runCommand(
+      commands.indentList,
+      markdown`
+        - A1
+          - B1
+          - <start>B2
+        - A2
+          - B3
+            - C1<end>
+              - D1
+          - B4
+      `,
+      markdown`
+        - A1
+          - B1
+            - <start>B2
+          - A2
+            - B3
+              - C1<end>
+              - D1
+          - B4
+      `,
+    )
+  })
 })
