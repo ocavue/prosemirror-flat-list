6867291f9a7a4390e0575660d41c4639408702a0
ocavue <ocavue@gmail.com>
2023-01-28 05:54:04 +0800

    move to same file

--- src/commands/dedent-list.ts
+++ src/commands/dedent-list.ts
@@ -11,7 +11,6 @@ import {
 } from '../utils/list-range'
 import { mapPos } from '../utils/map-pos'
 import { safeLift } from '../utils/safe-lift'
-import { indentListV3 } from './indent-list'
 import { separateItemRange } from './separate-item-range'
 
 export { createDedentListCommandV3 as createDedentListCommand }
@@ -350,5 +349,88 @@ function dedentOutOfList(tr: Transaction, range: NodeRange): boolean {
       true,
     ),
   )
+  return true
+}
+
+export function createIndentListCommandV3(listType: NodeType): Command {
+  const indentListCommand: Command = (state, dispatch): boolean => {
+    const tr = state.tr
+    if (indentListV3(tr, tr.selection.from, tr.selection.to, listType)) {
+      dispatch?.(tr)
+      return true
+    }
+    return false
+  }
+
+  return autoJoinList(indentListCommand, listType)
+}
+
+export function indentListV3(
+  tr: Transaction,
+  from: number,
+  to: number,
+  listType: NodeType,
+): boolean {
+  const map = mapPos(tr)
+
+  const $from = tr.doc.resolve(from)
+  const $to = tr.doc.resolve(to)
+  const range = findListsRange($from, $to, listType)
+  if (!range) return false
+
+  const leftOpenIndex = isLeftOpenRange(range)
+  const rightOpenIndex = isRightOpenRange(range)
+
+  let leftStart = leftOpenIndex !== false && $from.before(range.depth + 1) + 1
+  let leftEnd = leftOpenIndex !== false && $from.before($from.depth + 1) + 1
+
+  let rightStart = rightOpenIndex !== false && $to.after(range.depth + 1) - 1
+  let rightEnd = rightOpenIndex !== false && $to.after($to.depth - 1) - 1
+
+  indentListsRange(tr, range, listType)
+  // return true
+
+  leftStart = leftStart !== false && map(leftStart)
+  leftEnd = leftEnd !== false && map(leftEnd)
+  rightStart = rightStart !== false && map(rightStart)
+  rightEnd = rightEnd !== false && map(rightEnd)
+
+  console.log('indentListV3:', {
+    leftStart,
+    leftEnd,
+    rightStart,
+    rightEnd,
+  })
+
+  if (rightStart !== false && rightEnd !== false) {
+    dedentListV3(tr, rightStart, rightEnd, listType)
+  }
+
+  if (leftStart !== false && leftEnd !== false) {
+    dedentListV3(tr, leftStart, leftEnd, listType)
+  }
+
+  return true
+}
+
+function indentListsRange(
+  tr: Transaction,
+  range: NodeRange,
+  listType: NodeType,
+): boolean {
+  const { start, end } = range
+
+  tr.step(
+    new ReplaceAroundStep(
+      start,
+      end,
+      start,
+      end,
+      new Slice(Fragment.from(listType.create(null)), 0, 0),
+      1,
+      true,
+    ),
+  )
+
   return true
 }
