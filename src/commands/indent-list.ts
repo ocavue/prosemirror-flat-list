f5f2b18d2ecd4bfcf52a0ed84ecf261aa1b86f64
ocavue <ocavue@gmail.com>
2023-02-03 18:19:57 +0800

    bug fix

--- src/commands/indent-list.ts
+++ src/commands/indent-list.ts
@@ -1,7 +1,7 @@
 import { Fragment, NodeRange, NodeType, Slice } from '@remirror/pm/model'
 import { Command, Transaction } from '@remirror/pm/state'
 import { ReplaceAroundStep } from '@remirror/pm/transform'
-import { autoJoinList, autoJoinList2 } from '../utils/auto-join-list'
+import { autoJoinList2 } from '../utils/auto-join-list'
 import {
   atEndBlockBoundary,
   atStartBlockBoundary,
@@ -9,126 +9,8 @@ import {
 import { findListsRange } from '../utils/list-range'
 import { mapPos } from '../utils/map-pos'
 import { zoomInRange } from '../utils/zoom-in-range'
-import { separateItemRange } from './separate-item-range'
 
-export { createIndentListCommandV4 as createIndentListCommand }
-
-export function createIndentListCommandV1(listType: NodeType): Command {
-  /*
-
-  Let's say we have the following list nodes, represented in the Markdown
-  syntax. The <start> and <end> are the current selection range.
-
-  - A1
-    - B1
-    - <start>B2
-  - A2
-    - B3
-      - C1<end>
-        - D1
-    - B4
-
-  Here is what this function will do to indent the list nodes:
-
-  Step 1: we split these list nodes into three parts. (FYI, the following code snippet is a 100%
-  valid Markdown syntax based on the CommonMark spec).
-
-  - A1
-    - B1
-
-  - - <start>B2
-  - A2
-    - B3
-      - C1<end>
-
-  - - - - D1
-    - B4
-
-  Step 2: we need to increase the indentation for the middle part. 
-
-  - A1
-    - B1
-
-  - - - <start>B2
-    - A2
-      - B3
-        - C1<end>
-
-  - - - - D1
-    - B4
-
-  Step 3: we join all three parts together and get rid of the extra list nodes
-
-  - A1
-    - B1
-      - <start>B2
-    - A2
-      - B3
-        - C1<end>
-        - D1
-    - B4
-
-  Q&A
-
-  Q: Why don't we just use the `ReplaceAroundStep` to do the job?
-
-  A: We cannot, at least with the current `ReplaceAroundStep` design.
-  `ReplaceAroundStep` requires the gap to be a flat range
-  (https://github.com/prosemirror/prosemirror-transform/blob/1.7.1/src/replace_step.ts#L116),
-  while the slice between <start> and <end> in the example above is not flat.
-  We could, however, update the current `ReplaceAroundStep` to support such
-  cases, but I would like to avoid changing the core library for now.
-
-  Q: Why don't we just use one big `ReplaceStep`, to wrap the content that
-  need to be indented? 
-
-  A: Because we will lose the text selection, as the selection range will been
-  replaced if we do so.
-
-  */
-
-  const indentListCommand: Command = (state, dispatch): boolean => {
-    const tr = state.tr
-
-    {
-      const { $from, $to } = tr.selection
-      separateItemRange(tr, $from, $to, listType)
-    }
-
-    {
-      const { $from, $to } = tr.selection
-
-      const itemsRange = findListsRange($from, $to, listType)
-      if (!itemsRange) {
-        return false
-      }
-
-      const { start, end } = itemsRange
-
-      tr.step(
-        new ReplaceAroundStep(
-          start,
-          end,
-          start,
-          end,
-          new Slice(Fragment.from(listType.create(null)), 0, 0),
-          1,
-          true,
-        ),
-      )
-    }
-
-    if (dispatch) {
-      dispatch(tr)
-    }
-
-    return true
-  }
-
-  return autoJoinList(indentListCommand, listType)
-}
-
-export function createIndentListCommandV4(listType: NodeType): Command {
+export function createIndentListCommand(listType: NodeType): Command {
   const indentListCommand: Command = (state, dispatch): boolean => {
     const tr = state.tr
     const { $from, $to } = tr.selection
--- src/utils/auto-join-list.ts
+++ src/utils/auto-join-list.ts
@@ -1,22 +1,7 @@
 import { ProsemirrorNode } from '@remirror/core'
-import { autoJoin } from '@remirror/pm/commands'
-import { canJoin } from '@remirror/pm/transform'
 import { NodeType } from '@remirror/pm/model'
-import { Command, Transaction } from '@remirror/pm/state'
-
-/**
- * Wrap a command so that it can join two adjacent lists when necessary.
- */
-export function autoJoinList(command: Command, listType: NodeType): Command {
-  const isListJoinable = (before: ProsemirrorNode, after: ProsemirrorNode) => {
-    return (
-      before.type === listType &&
-      after.type === listType &&
-      after.firstChild?.type === listType
-    )
-  }
-  return autoJoin(command, isListJoinable)
-}
+import { Transaction } from '@remirror/pm/state'
+import { canJoin } from '@remirror/pm/transform'
 
 /** @internal */
 export function getTransactionRanges(tr: Transaction): number[] {
