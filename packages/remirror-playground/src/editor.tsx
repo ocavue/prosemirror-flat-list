7c33744f88704ad844a66aeb39169cdeba65077a
ocavue <ocavue@gmail.com>
2023-02-13 21:27:36 +0800

    chore: revert demo list

--- packages/remirror-playground/src/editor.tsx
+++ packages/remirror-playground/src/editor.tsx
@@ -163,10 +163,141 @@ const html = String.raw // Just for better editor support
 
 const content = html`
   <ul>
-    <li>
-      <p>A</p>
+    <li>first unordered list item</li>
+    <li>second unordered list item</li>
+  </ul>
+  <ol>
+    <li>first ordered list item</li>
+    <li>second ordered list item</li>
+  </ol>
+  <ul data-task-list>
+    <li data-task-list-item>first task list item</li>
+    <li data-task-list-item data-checked>second task list item</li>
+  </ul>
+
+  <ul data-toggle-list>
+    <li data-toggle-list-item>empty toggle list item</li>
+    <li data-toggle-list-item>
+      expanded toggle list item
       <ul>
-        <li>B</li>
+        <li>sub list item</li>
+        <li>sub list item</li>
+        <li>
+          sub list item
+
+          <ul data-task-list>
+            <li data-task-list-item>
+              list with multiple paragraphs
+              <p>paragraph</p>
+              <p>paragraph</p>
+            </li>
+          </ul>
+
+          <ul data-task-list>
+            <li data-task-list-item>
+              list with multiple paragraphs
+              <p>paragraph</p>
+              <p>paragraph</p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li data-toggle-list-item data-list-collapsed>
+      collapsed toggle list item
+      <ul>
+        <li>sub list item</li>
+        <li>sub list item</li>
+        <li>sub list item</li>
+        <li>sub list item</li>
+      </ul>
+    </li>
+  </ul>
+
+  <h2>Nested bullet list:</h2>
+
+  <ul>
+    <li>A</li>
+    <li>B</li>
+    <li>
+      C
+      <ul>
+        <li>C.A</li>
+        <li>
+          C.B
+          <ul>
+            <li>C.B.A</li>
+            <li>C.B.B</li>
+            <li>C.B.C</li>
+          </ul>
+        </li>
+        <li>C.C</li>
+      </ul>
+    </li>
+  </ul>
+
+  <h2>Nested ordered list:</h2>
+
+  <ol>
+    <li>A</li>
+    <li>B</li>
+    <li>
+      C
+      <ol>
+        <li>C.A</li>
+        <li>
+          C.B
+          <ol>
+            <li>C.B.A</li>
+            <li>C.B.B</li>
+            <li>C.B.C</li>
+          </ol>
+        </li>
+        <li>C.C</li>
+      </ol>
+    </li>
+  </ol>
+
+  <h2>Nested task list:</h2>
+
+  <ul data-task-list>
+    <li data-task-list-item data-checked>A</li>
+    <li data-task-list-item>B</li>
+    <li data-task-list-item data-checked>
+      C
+      <ul data-checked data-task-list>
+        <li data-task-list-item>C.A</li>
+        <li data-task-list-item>
+          C.B
+          <ul data-task-list>
+            <li data-task-list-item data-checked>C.B.A</li>
+            <li data-task-list-item data-checked>C.B.B</li>
+            <li data-task-list-item>C.B.C</li>
+          </ul>
+        </li>
+        <li>C.C</li>
+      </ul>
+    </li>
+  </ul>
+
+  <h2>Nested mixed list:</h2>
+
+  <ul>
+    <li>A</li>
+    <li>B</li>
+    <li>
+      C
+      <ul data-task-list>
+        <li data-task-list-item>C.A</li>
+        <li data-task-list-item data-checked>
+          C.B
+          <ol>
+            <li>C.B.A</li>
+            <li>C.B.B</li>
+            <li>C.B.C</li>
+          </ol>
+        </li>
+        <li data-task-list-item data-checked>C.C</li>
       </ul>
     </li>
   </ul>
