bbae47053029152bf21c5fee95d1b8d048654d51
ocavue <ocavue@gmail.com>
2023-02-16 01:40:12 +0800

    test: add test

--- packages/prosemirror-package/src/commands/move-list.spec.ts
+++ packages/prosemirror-package/src/commands/move-list.spec.ts
@@ -24,6 +24,26 @@ describe('moveList', () => {
     )
   })
 
+  it('can move up and dedent list nodes to parent list', () => {
+    t.runCommand(
+      moveUp,
+      markdown`
+        - A1
+        - A2
+          - B1<start>
+          - B2<end>
+          - B3
+      `,
+      markdown`
+        - A1
+        - B1<start>
+        - B2<end>
+        - A2
+          - B3
+      `,
+    )
+  })
+
   it('can move down list nodes', () => {
     t.runCommand(
       moveDown,
@@ -36,6 +56,26 @@ describe('moveList', () => {
         - A3
         - A1<start>
         - A2<end>
+      `,
+    )
+  })
+
+  it('can move down and dedent list nodes to parent list', () => {
+    t.runCommand(
+      moveUp,
+      markdown`
+        - A1
+        - A2
+          - B1<start>
+          - B2<end>
+        - A3
+      `,
+      markdown`
+        - A1
+        - A2
+        - A3
+        - B1<start>
+        - B2<end>
       `,
     )
   })
