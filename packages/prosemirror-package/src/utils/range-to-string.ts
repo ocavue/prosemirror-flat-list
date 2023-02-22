7f830645a9bae1423309f35f6c05ee31263f6988
ocavue <ocavue@gmail.com>
2023-02-22 21:09:26 +0800

    feat: add `rangeToString`

---
+++ packages/prosemirror-package/src/utils/range-to-string.ts
@@ -0,0 +1,12 @@
+import { NodeRange } from 'prosemirror-model'
+import { cutByIndex } from './cut-by-index'
+
+/**
+ * Return a debugging string that describes this range.
+ *
+ * @internal
+ */
+export function rangeToString(range: NodeRange): string {
+  const { parent, startIndex, endIndex } = range
+  return cutByIndex(parent.content, startIndex, endIndex).toString()
+}
