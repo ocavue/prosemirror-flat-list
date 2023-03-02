78201150eaa27dd60af0cd86f3170c01c7d985e9
ocavue <ocavue@gmail.com>
2023-03-02 19:57:14 +0800

    fix: remove remark from the comment

--- packages/core/src/plugins/index.ts
+++ packages/core/src/plugins/index.ts
@@ -8,8 +8,6 @@ import { createSafariInputMethodWorkaroundPlugin } from './safari-workaround'
 /**
  * This function returns an array of plugins that are required for list to work.
  *
- * @remark
- *
  * The plugins are shown below. You can pick and choose which plugins you want
  * to use if you want to customize some behavior.
  *
