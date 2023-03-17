ca48ce48c37dc252d83ff1161763b895289ba3d1
ocavue <ocavue@gmail.com>
2023-03-17 14:37:07 +0800

    docs: add loading text

--- website/components/editor-placeholder.tsx
+++ website/components/editor-placeholder.tsx
@@ -5,7 +5,9 @@ export const EditorPlaceholder: FC = () => {
   return (
     <ThemeProvider>
       <div className="remirror-editor-wrapper">
-        <div className="ProseMirror remirror-editor" />
+        <div className="ProseMirror remirror-editor">
+          <div>Loading...</div>
+        </div>
       </div>
     </ThemeProvider>
   )
