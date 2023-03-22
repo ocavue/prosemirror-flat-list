763c8ae1d1b8b8ae95edd069a310352131eeac3c
ocavue <ocavue@users.noreply.github.com>
2023-03-23 00:28:45 +0800

    docs: fix demo editor layout shifting (#92)

---
+++ website/components/editor-placeholder.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from '@remirror/react'
+import React, { FC } from 'react'
+
+export const EditorPlaceholder: FC = () => {
+  return (
+    <ThemeProvider>
+      <div className="remirror-editor-wrapper">
+        <div className="ProseMirror remirror-editor">
+          <div>Loading...</div>
+        </div>
+      </div>
+    </ThemeProvider>
+  )
+}
--- website/components/lazy-editor.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-export const Editor = dynamic(
-  import('./editor').then((module) => module.Editor),
-  {
-    loading: () => null,
-    ssr: false,
-  },
-)
