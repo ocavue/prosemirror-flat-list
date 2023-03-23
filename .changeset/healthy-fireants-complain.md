9c3d35ccfe8144bab8d3ede40f902cc9e4675211
ocavue <ocavue@gmail.com>
2023-03-23 22:07:47 +0800

    fix: paste deeply nested list without parent

---
+++ .changeset/healthy-fireants-complain.md
@@ -0,0 +1,5 @@
+---
+'prosemirror-flat-list': patch
+---
+
+Fix a bug that causes pasting a deeply nested list will bring too many indentations
--- packages/core/src/plugins/clipboard.ts
+++ packages/core/src/plugins/clipboard.ts
@@ -1,6 +1,7 @@
 import { Schema } from 'prosemirror-model'
 import { Plugin } from 'prosemirror-state'
 import { ListDOMSerializer } from '../utils/list-serializer'
+import { unwrapListSlice } from '../utils/unwrap-list-slice'
 
 /**
  * Serialize list nodes into native HTML list elements (i.e. `<ul>`, `<ol>`) to
@@ -12,6 +13,8 @@ export function createListClipboardPlugin(schema: Schema): Plugin {
   return new Plugin({
     props: {
       clipboardSerializer: ListDOMSerializer.fromSchema(schema),
+
+      transformCopied: unwrapListSlice,
     },
   })
 }
