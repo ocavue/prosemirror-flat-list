7b8a04da03daa754613e1e16dfe618b071029d72
ocavue <ocavue@users.noreply.github.com>
2024-07-03 09:39:04 +1000

    refactor: add prosemirror-safari-ime-span (#219)

--- packages/core/src/plugins/safari-workaround.ts
+++ packages/core/src/plugins/safari-workaround.ts
@@ -1,7 +1,5 @@
+import { imeSpan } from 'prosemirror-safari-ime-span'
 import { Plugin } from 'prosemirror-state'
-import { Decoration, DecorationSet, EditorView } from 'prosemirror-view'
-
-import * as browser from '../utils/browser'
 
 /**
  * Return a plugin as a workaround for a bug in Safari that causes the composition
@@ -12,37 +10,5 @@ import * as browser from '../utils/browser'
  * @public @group Plugins
  */
 export function createSafariInputMethodWorkaroundPlugin(): Plugin {
-  let view: EditorView | null = null
-  let span: HTMLSpanElement | null = null
-
-  const getSpan = (): HTMLSpanElement => {
-    if (!span) {
-      span = document.createElement('span')
-      span.className = 'prosemirror-flat-list-safari-workaround'
-    }
-    return span
-  }
-
-  const createDecorations = (): DecorationSet | null => {
-    if (view?.composing) {
-      const state = view.state
-      const selection = state.selection
-      const { $from, $to, to } = selection
-      if ($from.sameParent($to)) {
-        return DecorationSet.create(state.doc, [Decoration.widget(to, getSpan)])
-      }
-    }
-    return null
-  }
-
-  return new Plugin({
-    props: {
-      decorations: browser.safari ? createDecorations : undefined,
-    },
-
-    view: (v) => {
-      view = v
-      return {}
-    },
-  })
+  return imeSpan
 }
