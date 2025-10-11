646efd42b717f9fc674e16b3bffc57b1d5be58fa
ocavue <ocavue@gmail.com>
2025-10-11 19:07:43 +1100

    fix icon

--- website2/astro.config.mjs
+++ website2/astro.config.mjs
@@ -1,36 +1,37 @@
 // @ts-check
+
+import preact from '@astrojs/preact'
 import starlight from '@astrojs/starlight'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 import starlightThemeNova from 'starlight-theme-nova'
 
-import preact from '@astrojs/preact';
-
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'prosemirror-flat-list',
-    plugins: [
-      starlightThemeNova({
-        nav: [
-          { label: 'docs', href: '/introduction' },
-        ],
-      }),
-    ],
-    social: [
-      {
-        icon: 'github',
-        label: 'GitHub',
-        href: 'https://github.com/ocavue/prosemirror-flat-list',
-      },
-    ],
-    sidebar: [
-      { label: 'Introduction', slug: 'introduction' },
-      { label: 'Guide', slug: 'guides' },
-      { label: 'API Reference', slug: 'reference' },
-    ],
-  }), preact()],
+  integrations: [
+    starlight({
+      title: 'prosemirror-flat-list',
+      plugins: [
+        starlightThemeNova({
+          nav: [{ label: 'docs', href: '/introduction' }],
+        }),
+      ],
+      customCss: ['./src/styles/tailwindcss.css'],
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/ocavue/prosemirror-flat-list',
+        },
+      ],
+      sidebar: [
+        { label: 'Introduction', slug: 'introduction' },
+        { label: 'Guide', slug: 'guides' },
+        { label: 'API Reference', slug: 'reference' },
+      ],
+    }),
+    preact(),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
