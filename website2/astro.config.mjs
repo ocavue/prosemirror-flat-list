5b7db06c4da7f1fbb92fd239648cea14cfb1c84d
ocavue <ocavue@gmail.com>
2025-10-11 18:48:16 +1100

    Revert "add svelte"
    This reverts commit 61fad86a6b678057b857dbef113e525169b92ed3.

--- website2/astro.config.mjs
+++ website2/astro.config.mjs
@@ -3,30 +3,30 @@ import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
 import starlightThemeNova from 'starlight-theme-nova'
 
-import svelte from '@astrojs/svelte';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'prosemirror-flat-list',
-    plugins: [
-      starlightThemeNova({
-        nav: [
-          { label: 'docs', href: '/introduction' },
-        ],
-      }),
-    ],
-    social: [
-      {
-        icon: 'github',
-        label: 'GitHub',
-        href: 'https://github.com/ocavue/prosemirror-flat-list',
-      },
-    ],
-    sidebar: [
-      { label: 'Introduction', slug: 'introduction' },
-      { label: 'Guide', slug: 'guides' },
-      { label: 'API Reference', slug: 'reference' },
-    ],
-  }), svelte()],
+  integrations: [
+    starlight({
+      title: 'prosemirror-flat-list',
+      plugins: [
+        starlightThemeNova({
+          nav: [
+            { label: 'docs', href: '/introduction' },
+          ],
+        }),
+      ],
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/ocavue/prosemirror-flat-list',
+        },
+      ],
+      sidebar: [
+        { label: 'Introduction', slug: 'introduction' },
+        { label: 'Guide', slug: 'guides' },
+        { label: 'API Reference', slug: 'reference' },
+      ],
+    }),
+  ],
 })
