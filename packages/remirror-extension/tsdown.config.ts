cb5bd17acc683b461d066780b4c4f12f06c4db5d
ocavue <ocavue@gmail.com>
2025-10-11 21:01:15 +1100

    fix build

--- packages/remirror-extension/tsdown.config.ts
+++ packages/remirror-extension/tsdown.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   format: ['cjs', 'esm'],
-  entry: { 'prosemirror-flat-list': 'src/index.ts' },
+  entry: { 'remirror-extension-flat-list': 'src/index.ts' },
   dts: { build: true },
 })
