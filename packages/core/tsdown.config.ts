10a9347498305ffe4006bc907d97b864ccec0eff
ocavue <ocavue@gmail.com>
2025-10-11 20:33:50 +1100

    remove soucemap

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   format: ['cjs', 'esm'],
-  sourcemap: false,
   entry: { 'prosemirror-flat-list': 'src/index.ts', style: 'src/style.css' },
   dts: { build: true },
 })
