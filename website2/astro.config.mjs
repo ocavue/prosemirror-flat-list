07f571cba926b583dc35255f1b051e6acbcb9897
ocavue <ocavue@gmail.com>
2025-10-11 18:32:07 +1100

    install

--- website2/astro.config.mjs
+++ website2/astro.config.mjs
@@ -1,26 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from '@astrojs/starlight'
+import starlightThemeNova from '@starlightjs/theme-nova'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+  integrations: [
+    starlight({
+      title: 'prosemirror-flat-list',
+      plugins: [
+        starlightThemeNova(
+          { label: 'Quick start', href: '/guide/getting-started' },
+          { label: 'External link', href: 'https://example.com' },
+        ),
+      ],
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/withastro/starlight',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+})
