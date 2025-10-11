bd3fe74f0bfac716ae76d281e5e01cecc0a61cbf
ocavue <ocavue@gmail.com>
2025-10-11 18:49:22 +1100

    add preact

--- website2/astro.config.mjs
+++ website2/astro.config.mjs
@@ -3,30 +3,30 @@ import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
 import starlightThemeNova from 'starlight-theme-nova'
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'prosemirror-flat-list',
-      plugins: [
-        starlightThemeNova({
-          nav: [
-            { label: 'docs', href: '/introduction' },
-          ],
-        }),
-      ],
-      social: [
-        {
-          icon: 'github',
-          label: 'GitHub',
-          href: 'https://github.com/ocavue/prosemirror-flat-list',
-        },
-      ],
-      sidebar: [
-        { label: 'Introduction', slug: 'introduction' },
-        { label: 'Guide', slug: 'guides' },
-        { label: 'API Reference', slug: 'reference' },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: 'prosemirror-flat-list',
+    plugins: [
+      starlightThemeNova({
+        nav: [
+          { label: 'docs', href: '/introduction' },
+        ],
+      }),
+    ],
+    social: [
+      {
+        icon: 'github',
+        label: 'GitHub',
+        href: 'https://github.com/ocavue/prosemirror-flat-list',
+      },
+    ],
+    sidebar: [
+      { label: 'Introduction', slug: 'introduction' },
+      { label: 'Guide', slug: 'guides' },
+      { label: 'API Reference', slug: 'reference' },
+    ],
+  }), preact()],
 })
