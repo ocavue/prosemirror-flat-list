8998ee78c6c2cbf4654a4d21b0d58dacaca45143
ocavue <ocavue@gmail.com>
2026-05-17 09:36:37 +1000

    fix

--- website/src/content.config.ts
+++ website/src/content.config.ts
@@ -2,6 +2,6 @@ import { docsLoader } from '@astrojs/starlight/loaders'
 import { docsSchema } from '@astrojs/starlight/schema'
 import { defineCollection } from 'astro:content'
 
-export const collections = {
+export const collections: unknown = {
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 }
