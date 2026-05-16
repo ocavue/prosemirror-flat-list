f53e112a5f49ad7c68df318e13c7e7efdba5286d
ocavue <ocavue@gmail.com>
2026-05-17 09:32:12 +1000

    fix prosekit

--- website/src/components/demo-editor/button.tsx
+++ website/src/components/demo-editor/button.tsx
@@ -1,41 +1,43 @@
-import type { ReactNode } from 'preact/compat'
+import type { ComponentChild, MouseEventHandler } from 'preact'
 import {
-  TooltipContent,
+  TooltipPopup,
+  TooltipPositioner,
   TooltipRoot,
   TooltipTrigger,
 } from 'prosekit/preact/tooltip'
 
-export default function Button({
-  pressed,
-  disabled,
-  onClick,
-  tooltip,
-  children,
-}: {
+export default function Button(props: {
   pressed?: boolean
   disabled?: boolean
-  onClick?: VoidFunction
+  onClick?: MouseEventHandler<HTMLButtonElement>
   tooltip?: string
-  children: ReactNode
+  children: ComponentChild
 }) {
   return (
     <TooltipRoot>
       <TooltipTrigger className="block">
         <button
-          data-state={pressed ? 'on' : 'off'}
-          disabled={disabled}
-          onClick={() => onClick?.()}
-          onMouseDown={(event) => event.preventDefault()}
+          data-state={props.pressed ? 'on' : 'off'}
+          disabled={props.disabled}
+          onClick={props.onClick}
+          onMouseDown={(event) => {
+            // Prevent the editor from being blurred when the button is clicked
+            event.preventDefault()
+          }}
           className="outline-unset focus-visible:outline-unset flex items-center justify-center rounded-md p-2 font-medium transition focus-visible:ring-2 text-sm focus-visible:ring-gray-900 dark:focus-visible:ring-gray-300 disabled:pointer-events-none min-w-9 min-h-9 text-gray-900 dark:text-gray-50 disabled:text-gray-900/50 dark:disabled:text-gray-50/50 bg-transparent hover:bg-gray-100 dark:hover:bg-gray-800 data-[state=on]:bg-gray-200 dark:data-[state=on]:bg-gray-700"
         >
-          {children}
-          {tooltip ? <span className="sr-only">{tooltip}</span> : null}
+          {props.children}
+          {props.tooltip ? (
+            <span className="sr-only">{props.tooltip}</span>
+          ) : null}
         </button>
       </TooltipTrigger>
-      {tooltip ? (
-        <TooltipContent className="z-50 overflow-hidden rounded-md border border-solid bg-gray-900 dark:bg-gray-50 px-3 py-1.5 text-xs text-gray-50 dark:text-gray-900 shadow-xs [&:not([data-state])]:hidden will-change-transform data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 data-[state=open]:zoom-in-95 data-[state=closed]:zoom-out-95 data-[state=open]:animate-duration-150 data-[state=closed]:animate-duration-200 data-[side=bottom]:slide-in-from-top-2 data-[side=bottom]:slide-out-to-top-2 data-[side=left]:slide-in-from-right-2 data-[side=left]:slide-out-to-right-2 data-[side=right]:slide-in-from-left-2 data-[side=right]:slide-out-to-left-2 data-[side=top]:slide-in-from-bottom-2 data-[side=top]:slide-out-to-bottom-2">
-          {tooltip}
-        </TooltipContent>
+      {props.tooltip ? (
+        <TooltipPositioner className="block overflow-visible w-min h-min z-50 ease-out transition-transform duration-100 motion-reduce:transition-none">
+          <TooltipPopup className="flex box-border origin-(--transform-origin) transition-[opacity,scale] transition-discrete motion-reduce:transition-none duration-100 data-[state=closed]:duration-150 data-[state=closed]:opacity-0 starting:opacity-0 data-[state=closed]:scale-95 starting:scale-95 overflow-hidden rounded-md border border-solid bg-gray-900 dark:bg-gray-50 px-3 py-1.5 text-xs text-gray-50 dark:text-gray-900 shadow-xs text-nowrap">
+            {props.tooltip}
+          </TooltipPopup>
+        </TooltipPositioner>
       ) : null}
     </TooltipRoot>
   )
