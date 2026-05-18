ec297e3ceaeecd9f84afc9add06da042a7d98eee
ocavue <ocavue@gmail.com>
2026-05-18 16:10:02 +1000

    fix

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig([
-  {
-    format: ['esm'],
-    entry: ['src/index.ts', 'src/style.css'],
-    target: 'chrome100',
-    dts: { build: true, incremental: true },
-    platform: 'browser',
-  }
-])
+export default defineConfig({
+  format: ['esm'],
+  entry: ['src/index.ts', 'src/style.css'],
+  target: 'chrome100',
+  dts: { build: true },
+  platform: 'browser',
+})
