f4573ba8f4cef1763d718c6f0f37d595a5ee4171
ocavue <ocavue@gmail.com>
2026-08-10 12:26:32 +1000

    format

--- packages/core/src/utils/is-list-node.ts
+++ packages/core/src/utils/is-list-node.ts
@@ -2,7 +2,9 @@ import type { Node as ProsemirrorNode } from 'prosemirror-model'
 
 import { isListType } from './is-list-type'
 
-/** @public */
+/**
+ * @public
+ */
 export function isListNode(node: ProsemirrorNode | null | undefined): boolean {
   if (!node) return false
   return isListType(node.type)
--- packages/core/src/utils/is-list-type.ts
+++ packages/core/src/utils/is-list-type.ts
@@ -2,7 +2,9 @@ import type { NodeType } from 'prosemirror-model'
 
 import { getListTypeName } from './get-list-type-name'
 
-/** @public */
+/**
+ * @public
+ */
 export function isListType(type: NodeType): boolean {
   return getListTypeName(type.schema) === type.name
 }
